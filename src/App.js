f74daa713846a226799d49eaef859a8f5587dbef
Rocketman <abhinayshambharkar1@gmail.com>
2025-04-06 17:30:20 +0530

    practice

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import { useState } from "react";
-import "./styles.css";
-import explorer from "./data/folderData";
-import Folder from "./components/Folder";
-import useTraverseTree from "./hooks/use-traverse-tree";
+import React, { useState } from 'react'
+import explorer from './data/folderData'
+import Folder from './components/Folder';
+import './styles.css'
+import useTraverseTree from './hooks/use-traverse-tree';
 
-export default function App() {
-  const [explorerData, setExplorerData] = useState(explorer);
+const App = () =>{
+    const[explorerData,setExplorerData] = useState(explorer);
+    const { insertNode } = useTraverseTree();
 
-  const { insertNode } = useTraverseTree();
+    const handleInsertNode = (folderId,item,isFolder) =>{
+        const finalTree = insertNode(explorerData,folderId,item,isFolder);
+        setExplorerData(finalTree);
+    }
 
-  const handleInsertNode = (folderId, item, isFolder) => {
-    const finalTree = insertNode(explorerData, folderId, item, isFolder);
-    setExplorerData(finalTree);
-  };
-
-  return (
-    <div className="App">
-      <Folder handleInsertNode={handleInsertNode} explorer={explorerData} />
-    </div>
-  );
+    return (
+        <div>
+            <Folder explorer={explorerData} handleInsertNode={handleInsertNode}/>
+        </div>
+    )
 }
+
+export default App;
--- src/components/Folder.js
+++ src/components/Folder.js
@@ -1,70 +1,56 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
-const Folder = ({ handleInsertNode, explorer }) => {
-  const [expand, setExpand] = useState(false);
-  const [showinput, setShowInput] = useState({
-    visible: false,
-    isFolder: null,
-  });
+export default function Folder({explorer, handleInsertNode}){
+    const[expand,setExpand] = useState(false);
+    const[showInput,setShowInput] = useState({
+        visible:false,
+        isFolder:null
+    })
 
-  const handleNewFolder = (e, isFolder) => {
-    e.stopPropagation();
-    setShowInput({
-      visible: true,
-      isFolder,
-    });
-    setExpand(true);
-  };
-
-  const onAddFolder = (e) => {
-    if (e.keyCode === 13 && e.target.value) {
-      // add logic
-      handleInsertNode(explorer.id, e.target.value, showinput.isFolder);
-      setShowInput({ ...showinput, visible: false });
+    function handleNewFolder(e,isFolder){
+        e.stopPropagation();
+        setExpand(true);
+        setShowInput({visible:true,isFolder});
     }
-  };
 
-  if (explorer.isFolder) {
-    return (
-      <div>
-        <div className="folder" onClick={() => setExpand(!expand)}>
-          <span>📂 {explorer.name}</span>
-          <div>
-            <button onClick={(e) => handleNewFolder(e, true)}>Folder+ </button>
-            <button onClick={(e) => handleNewFolder(e, false)}>File+</button>
-          </div>
-        </div>
-        <div
-          style={{ display: expand ? "block" : "none", paddingLeft: "25px" }}
-        >
-          {showinput.visible && (
-            <div className="inputContainer">
-              <span>{showinput.isFolder ? "📂" : "📄"}</span>
-              <input
-                type="text"
-                placeholder="enter name.."
-                className="inputContainer_input"
-                autoFocus
-                onBlur={() => setShowInput({ ...showinput, visible: false })}
-                onKeyDown={onAddFolder}
-              />
+    function onAddFolder(e){
+        if(e.keyCode === 13 && e.target.value){
+            handleInsertNode(explorer.id,e.target.value,showInput.isFolder);
+            setShowInput({...showInput,visible:false});
+        }
+    }
+
+    if(explorer.isFolder){
+        return (
+            <div className="wrapper">
+                <div className="folder" onClick={()=>setExpand(!expand)}>
+                    <span>📂{explorer.name}</span>
+                    <div>
+                        <button onClick={(e)=>handleNewFolder(e,true)}>Folder+</button>
+                        <button onClick={(e)=>handleNewFolder(e,false)}>File+</button>
+                    </div>
+                </div>
+                <div style={{ display:expand?"block":"none",paddingLeft:25 }}>
+                    {
+                        showInput.visible && <div className="input-container">
+                            <span>{showInput.isFolder?"📂":"📄"}</span>
+                            <input
+                            type="text"
+                            autoFocus
+                            onBlur={()=>setShowInput({...showInput,visible:false})}
+                            onKeyDown={onAddFolder}
+                            />
+                        </div>
+                    }
+                    {
+                        explorer.items.map((exp,ind)=>{
+                            return <Folder key={ind} explorer={exp} handleInsertNode={handleInsertNode} />
+                        })
+                    }
+                </div>
             </div>
-          )}
-          {explorer.items.map((exp) => {
-            return (
-              <Folder
-                handleInsertNode={handleInsertNode}
-                explorer={exp}
-                key={exp.id}
-              />
-            );
-          })}
-        </div>
-      </div>
-    );
-  } else {
-    return <span className="file">📄 {explorer.name}</span>;
-  }
-};
-
-export default Folder;
+        )
+    }else{
+        return <span className="file">📄{explorer.name}</span>
+    }
+}
